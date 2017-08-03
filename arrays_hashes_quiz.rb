400cb7e4db25ccc741a416db0d3d467c58858f99
Ryan Forgie <ryan.j.forgie@gmail.com>
2017-08-03 10:04:27 +0100

    Clean up syntax

--- arrays_hashes_quiz.rb
+++ arrays_hashes_quiz.rb
@@ -12,28 +12,29 @@ puts lines[1]
 puts lines[4]
 puts lines.last
 puts lines [-1]
+puts lines.pop
 
 # 4. Work out the index position of 'Haymarket'
 puts lines.index("Haymarket")
 
  # 5. Add 'Airport' to the start of the array
 lines_1 = lines.unshift("Airport")
-print lines_1
+puts lines.first
 
 # 6. Add 'York Place' to the end of the array
 lines_2 = lines.push("York Place")
-print lines_2
+puts lines.last
 
 # 7. Remove 'Edinburgh Park' from the array using it's name
 lines_3 = lines.delete("Edinburgh Park")
-print lines_3
+puts lines_3
 
 # 8. Delete 'Edinburgh Park' from the array by index
 lines_4 = lines.delete_at(1)
-print lines_4
+puts lines_4
 
 # 9. Reverse the positions of the stops in the array
-puts lines.reverse()
+puts lines.reverse!()
 
 # B. Given the following data structure:
 my_hash = {"0" => "Zero", 1 => "One", :two => "Two", "two" => 2}
@@ -49,11 +50,11 @@ puts my_hash["two"]
 
 # 4. How would you add `{3 => "Three"}` to the hash?
 my_hash[3] = "Three"
-print my_hash
+puts my_hash
 
 # 5. How would you add `{:four => 4}` to the hash?
 my_hash[:four] = 4
-print my_hash
+puts my_hash
 
 # C. Given the following data structure:
 
@@ -96,37 +97,37 @@ puts users["Jonathan"][:twitter]
 puts users["Erik"][:home_town]
 
 # 3. Return the array of Erik's favorite numbers
-print users["Erik"][:favourite_numbers]
+puts users["Erik"][:favourite_numbers]
 
 # 4. Return the type of Avril's pet Colin
 puts users["Avril"][:pets]["colin"]
 
 # 5. Return the smallest of Erik's favorite numbers
 smallest = users["Erik"][:favourite_numbers]
-puts smallest.min
+puts smallest.min()
 
 # 6. Add the number `7` to Erik's favorite numbers
-users["Erik"][:favourite_numbers].push 7
-print users["Erik"][:favourite_numbers]
+users["Erik"][:favourite_numbers].push(7)
+puts users["Erik"][:favourite_numbers]
 
 # 7. Change Erik's hometown to Edinburgh
 users["Erik"][:home_town] = "Edinburgh"
-print users["Erik"][:home_town]
+puts users["Erik"][:home_town]
 
 # 8. Add a pet dog to Erik called "Fluffy"
 users["Erik"][:pets]["Fluffy"] = :dog
-print users["Erik"][:pets]
+puts users["Erik"][:pets]
 
 # 9. Add yourself to the users hash
 ryan = {
   "Ryan" => {
-    :twitter => "ryanforgie",
-    :favourite_numbers => [1, 2, 3, 4],
-    :home_town => "Aberdeen",
-    :pets => {
+    twitter: "ryanforgie",
+    favourite_numbers: [1, 2, 3, 4],
+    home_town: => "Aberdeen",
+    pets: {
       "sunny" => :canary
 },
 }
 }
 
-print users.merge(ryan)
+puts users.merge(ryan)
